df35fef5a5d77e1564a390fe29f6b0adcea216bf
reimanning <reimanning@icloud.com>
2026-07-24 15:53:58 -0400

    Added basic functionality of pokemonMsg. Missing optional fields and proper output formatting

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ class programManager {
     response = `${response}`
     if (response.slice(0, 12) !== "|updateuser|" && response.slice(0,10) !== "|challstr|") {
       console.log(TEXT_LINE);
-      console.log(`[server]: ${response}`);
+      // console.log(`[server]: ${response}`);
       console.log(this.parseServerResponse(response));
       console.log(TEXT_LINE);
     }
--- src/pokemonMsg.js
+++ src/pokemonMsg.js
@@ -21,20 +21,58 @@ export class PokemonMsg extends Message {
   }
 
   parseMessage() {
+    //console.log(this.message);
+
     // 1. Get type(s)
-    let regexp = new RegExp("types\/(\w*)", "g");
-    let results = [...this.message.matchAll(regexp)];
-    this.types = [results[0][0], results[1][0]];
+    let regex = /(\/types\/(?<type>\w*))/g;
+    let results = [...this.message.matchAll(regex)];
+    if (results.length > 1) {
+      this.types = results[0].groups.type + "/" + results[1].groups.type;
+    } 
+    else {
+      this.types = results[0].groups.type
+    }
+
     // 2. Get smogon link
-    regexp = new RegExp("(https://dex.pokemonshowdown.com\/pokemon\/\w*)", "g");
-    results = this.message.match(regexp);
-    this.dexLink = results[0];
-    
+    regex = /(?<link>https:\/\/dex.pokemonshowdown.com\/pokemon\/\w+)/g;
+    //regexp = new RegExp("(?<link>https:\/\/dex.pokemonshowdown.com\/pokemon\/(?<name>\w+))", "g");
+    results = regex.exec(this.message);
+    this.dexLink = results.groups.link;
+
     // 3. Strip html tags for easier parsing
     const striptags = new RegExp("<[^>]*>", "g");
     const replaceStr = '';
     this.message = this.message.replaceAll(striptags, replaceStr);
-    return this.message;
+    // 4. Get all non-optional fields
+    regex = /\/raw (?<tier>\w+)  (?<name>\w+(?: \w+)?)  (?<ability1>[A-Z]{1}[a-z]+(?: [A-Z]{1}[a-z]+)?)(?<ability2>[A-Z]{1}[a-z]+(?: [A-Z]{1}[a-z]+)?)?(?<ability3>[A-Z]{1}[a-z]+(?: [A-Z]{1}[a-z]+)?)?.*?HP(?<HP>[0-9]+) Atk(?<Atk>[0-9]+) Def(?<Def>[0-9]+) SpA(?<SpA>[0-9]+) SpD(?<SpD>[0-9]+) Spe(?<Spe>[0-9]+) BST(?<BST>[0-9]+)[\s\S]*Dex\#: (?<dexNo>[0-9]+).*Gen: (?<genNo>[0-9]+).*Height: (?<height>[0-9\.\w ]+).*Weight: (?<weight>[^\)]*\)).*Dex Colour: (?<dexColour>\w*).*Egg Group\(s\): (?<eggGroups>[^\&]*)/g;
+    results = regex.exec(this.message);
+    let groups = results.groups;
+    this.tier = groups.tier;
+    this.name = groups.name;
+    this.ability1 = groups.ability1;
+    if (groups.ability3) {
+      this.ability2 = groups.ability2;
+      this.abilityHidden = groups.ability3;
+    }
+    else if (groups.ability2) {
+      this.abilityHidden = groups.ability2;
+    }
+    this.stats = [groups.HP, groups.Atk, groups.Def, groups.SpA, groups.SpD, groups.Spe, groups.BST];
+    this.dexNo = groups.dexNo;
+    this.genNo = groups.genNo;
+    this.height = groups.height;
+    this.weight = groups.weight;
+    this.dexColour = groups.dexColour;
+    this.eggGroups = groups.eggGroups;
+    this.printInfo();
+    return '';
+  }
+
+  printInfo() {
+    const WS = ' '
+    console.log(this.tier + WS + this.name + WS + this.types + WS + this.ability1 + this.ability2 + this.abilityHidden)
+    console.log(this.stats);
+    console.log(this.dexNo + WS + this.genNo + WS + this.height + WS + this.weight + WS + this.dexColour + WS + this.eggGroups);
   }
 }
 
